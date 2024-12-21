0e31119bda93230dff4f5ec1f87e131be2f2fbd0
KAL_codes <kal.codes00@gmail.com>
2024-12-21 10:45:50 +0100

    Update forms.py

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,7 +1,5 @@
 from django import forms
 from .models import UserProfile
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
 
 
 class UserProfileForm(forms.ModelForm):
